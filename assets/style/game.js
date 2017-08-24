d3d23505036b6d025c891b6822c6f7760d45c16b
unknown <mgdaye@gmail.com>
2017-08-23 20:19:41 -0400

    recreated arrays

--- assets/style/game.js
+++ assets/style/game.js
@@ -1,68 +1,93 @@
 //these variables will hold the hangman words
-var wordBank = {
-	washingtonDc : "washington dc",
-	oregon : "oregon",
-	michigan : "michigan",
-	whiteHouse : "white house"
-},
-	//number of wins
-	wins = 0,
+var wordBank = ["michigan", "oregon", "ohio", "mississippi"]
+    //number of wins
+    wins = 0,
 
-	//remaining gusses
-	guessesRemaining = 13,
+    //remaining gusses
+    guessesRemaining = 13,
 
-	//test array
-	testArray = ["h","a", "p", "p", "y"],
+    //test array
+    testArray = ["h", "a", "p", "p", "y"],
 
-	//this variable holds the user's input
-	userInput = [],
+    //this variable holds the user's input 
+    userInput = [],
 
-	//this variable holds guessed letters
-	guessedLetters = [];
+    //this variable holds guessed letters
+    guessedLetters = [],
+
+    //current word is the variable to match 
+    currentWord =  wordBank[Math.floor(Math.random() * wordBank.length)],
+
+    //this array will be fed input when the user makes a correct guess
+    correctGuesses = [];
 
 function gameWin() {
-	wins++;
+    wins++;
+    alert("We\'re going to " + currentWord + ". Byaaahh!");
 }
 
 function wrongGuess() {
-	gussesRemaining--;
+    guessesRemaining--;
 }
 
-function gamereset() {
-	guessesRemaining = 13;
-	guessedLetters = []
+function gameReset() {
+    guessesRemaining = 13;
+    guessedLetters = [];
+    currentWord = wordBank[Math.floor(Math.random() * wordBank.length)]
 }
 
 //this function starts the game when you press any butotn	
 document.onkeyup = function(event) {
-	userInput = event.key;
+    userInput = event.key;
 
-	//convert the key userInput into a string
-	//var userString = String.fromCharCode(userInput);
+    //convert the key userInput into a string
+    var userString = String.fromCharCode(userInput);
 
-	//if then statement that determines wheter or not a guess has already been placed
-	if (guessedLetters.indexOf(userInput) < 0) {
-		//this adds the inputs to the guessedLetters array
-		guessedLetters.push(userInput);
-	} else {
-		alert('you already guessed that!')
-	}
+    //if then statement that determines wheter or not a guess has already been placed
+    if (guessedLetters.indexOf(userInput) < 0) {
 
-	//this give the guessedLetters array output on keyup
-	console.log(guessedLetters)
+        //this adds the inputs to the guessedLetters array
+        guessedLetters.push(userInput);
+    } else {
+        alert('you already guessed that!');
+    }
 
-	//game output after key input
-	var html =
-		"<h1>Howard Dean Hangman</h1>" +
-		"<br>" +
-		"<br>" +
-		"<p>Current Word</p>" + word +
-		"<br>" +
-		"<br>" +
-		"<p>Guesses Remaining: "  + guessesRemaining +
-		"<br>" +
-		"<br>" +
-		"<p>Guessed Letters: " + guessedLetters ;
-	document.querySelector("#game").innerHTML = html;
+    //if you make a correct guess this pushes your input to the correct guess array
+    if (currentWord.indexOf(userInput) >= 0) {
+        correctGuesses.push(userInput);
+    } else {
+        wrongGuess();
+        if (guessesRemaining === 0) {
+            gameReset();
+        }
+    }
 
+    //this checks to see if the user wins by sorting characters in both the correct guesses and current word array
+    // if (correctGuesses.sort().join(",") === currentWord.sort().join(",")) {
+    //     gameWin();
+    //     gameReset();
+    // }
+
+    if (correctGuesses === currentWord) {
+    	gameWin();
+    	gameReset();
+    }
+
+
+    //this give the guessedLetters array output on keyup
+    console.log(guessedLetters)
+
+    //game output after key input
+    var html =
+        "<h1>Howard Dean Hangman</h1>" +
+        "<br>" +
+        "<br>" +
+        "<p>Current Word</p>" + currentWord +
+        "<br>" +
+        "<br>" +
+        "<p>Guesses Remaining: " + guessesRemaining +
+        "<br>" +
+        "<br>" +
+        "<p>Guessed Letters: " + guessedLetters;
+    document.querySelector("#game").innerHTML = html;
 }
